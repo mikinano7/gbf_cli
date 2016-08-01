f66c17a4aea2766af0cbc588b8222b17d45298f9
mikinano7 <halx25@gmail.com>
2016-08-01 23:28:20 +0900

    IDがコピーできない不具合を修正

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function printArrayElement(tweet, index, array) {
             keyElement.prepend('<p class="time">' + new Date(tweet.created_at).toLocaleString() + '</p>');
             keyElement.prepend('<p id="tw_' + tweet.id_str + '"></p>');
             $('#tw_' + tweet.id_str).text('@' + tweet.user.screen_name + ': ' + tweet.text);
-            keyElement.prepend('<span>' + helpNum + '</span>');
+            keyElement.prepend('<span class="' + helpNum + '">' + helpNum + '</span>');
             keyElement.prepend('<button id="' + helpNum + '" class="copy">copy</button>');
         }
     }, multi);
