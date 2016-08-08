cf4bd493dce35d3dda9dfbb673d8d624348209e1
mikinano7 <halx25@gmail.com>
2016-08-08 12:00:30 +0900

    四象対応

--- setting.js
+++ setting.js
@@ -1,22 +1,35 @@
 var $ = jQuery = require("./jquery-2.1.4.min.js");
 const ipcRenderer = require('electron').ipcRenderer;
 
-$(function(){
-    $.getJSON('resources/multi.json', function(json){
+$(function () {
+    $.getJSON('resources/multi.json', function (json) {
         var form = $('#multi');
-        json.forEach(function(value, index, array){
-            form.prepend(value + ': <input type="checkbox" class="select" name="'+value+'" /><br />');
+        json.forEach(function (value) {
+            form.prepend(value + ': <input type="checkbox" class="select" name="' + value + '" /><br />');
         })
     })
-})
+});
 
-$(document).on('click', '#setting_submit', function(event){
-    var form = $('#multi');
+$(document).on('click', '#setting_submit', function (event) {
     var setting = [];
-    form.children('.select').each(function(checkbox){
+    $('.select').each(function () {
         if ($(this).prop('checked')) setting.push($(this).attr('name'));
     });
 
     ipcRenderer.send('setting_submit', setting);
     event.preventDefault();
+});
+
+$(document).on('click', '#setting_sisyo', function () {
+    if ($(this).prop('checked')) {
+        $.getJSON('resources/sisyo.json', function (json) {
+            var form = $('#multi');
+            form.prepend('<div id="sisyo"></div>');
+            json.forEach(function (value) {
+                $('#sisyo').prepend(value + ': <input type="checkbox" class="select" name="' + value + '" /><br />');
+            })
+        })
+    } else {
+        $('#sisyo').empty();
+    }
 });
